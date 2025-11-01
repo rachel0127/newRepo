9eca70b9fd40a614de975c7129b7de3272858c61
rachel0127 <dio.e.grazioso227@gmail.com>
2025-11-01 12:03:24 +0530

    basics2.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("practice")
+let name = "Joanne";
+let age = 18;
+//console.log(name)
+//console.log(age)
+//console.log(name , age);
+//console.table([name , age]);
+//console.log(typeof age)
+
+let a = 2;
+let b = "2";
+console.log(a == b);
+console.log(a == b);
+
+
+
